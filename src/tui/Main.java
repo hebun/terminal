6282577df5dd2bf72a885b97e9096af0d17fbfba
Lazy Coding <itung73@gmail.com>
2021-04-21 16:44:50 +0300

    typo

--- src/tui/Main.java
+++ src/tui/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Terminal ter = new Terminal();
 		ter.move(20, 20);
-		ter.print("this should apear on 20:20");
+		ter.print("this should appear on 20:20");
 		ter.renderBlock("my block", new Rectangle(30, 25, 100, 20));
 		ter.move(32, 28);
 		ter.print("inside the block");
